b1c69123ff229adbc9678add697e2e83d3506e80
Nick Nyburg <nick@Mac-2590.lan>
2026-05-26 20:23:20 -0700

    Implemented inode specific testing

--- testfs.c
+++ testfs.c
@@ -81,6 +81,138 @@ static int test_find_free(void)
     return 0;
 }
 
+static int test_incore_find_free(void)
+{
+    struct inode *in;
+
+    incore_free_all();
+
+    in = incore_find_free();
+
+    CTEST_ASSERT(in != NULL);
+    CTEST_ASSERT(in->ref_count == 0);
+
+    return 0;
+}
+
+static int test_incore_find(void)
+{
+    struct inode *in;
+
+    incore_free_all();
+
+    in = incore_find_free();
+
+    CTEST_ASSERT(in != NULL);
+
+    in->ref_count = 1;
+    in->inode_num = 42;
+    in->size = 1234;
+
+    CTEST_ASSERT(incore_find(42) == in);
+    CTEST_ASSERT(incore_find(99) == NULL);
+
+    return 0;
+}
+
+static int test_read_write_inode(void)
+{
+    struct inode write_in;
+    struct inode read_in;
+    int i;
+
+    CTEST_ASSERT(image_open(TEST_IMAGE, 1) >= 0);
+
+    memset(&write_in, 0, sizeof(write_in));
+    memset(&read_in, 0, sizeof(read_in));
+
+    write_in.size = 123456;
+    write_in.owner_id = 25;
+    write_in.permissions = 7;
+    write_in.flags = 3;
+    write_in.link_count = 2;
+    write_in.inode_num = 5;
+
+    for (i = 0; i < INODE_PTR_COUNT; i++) {
+        write_in.block_ptr[i] = (unsigned short)(100 + i);
+    }
+
+    write_inode(&write_in);
+    read_inode(&read_in, 5);
+
+    CTEST_ASSERT(read_in.size == 123456);
+    CTEST_ASSERT(read_in.owner_id == 25);
+    CTEST_ASSERT(read_in.permissions == 7);
+    CTEST_ASSERT(read_in.flags == 3);
+    CTEST_ASSERT(read_in.link_count == 2);
+    CTEST_ASSERT(read_in.inode_num == 5);
+
+    for (i = 0; i < INODE_PTR_COUNT; i++) {
+        CTEST_ASSERT(read_in.block_ptr[i] == (unsigned short)(100 + i));
+    }
+
+    CTEST_ASSERT(image_close() == 0);
+
+    unlink(TEST_IMAGE);
+
+    return 0;
+}
+
+static int test_iget_iput(void)
+{
+    struct inode write_in;
+    struct inode *in1;
+    struct inode *in2;
+    struct inode read_in;
+
+    CTEST_ASSERT(image_open(TEST_IMAGE, 1) >= 0);
+
+    incore_free_all();
+
+    memset(&write_in, 0, sizeof(write_in));
+    memset(&read_in, 0, sizeof(read_in));
+
+    write_in.size = 888;
+    write_in.owner_id = 12;
+    write_in.permissions = 5;
+    write_in.flags = 1;
+    write_in.link_count = 3;
+    write_in.inode_num = 9;
+    write_in.block_ptr[0] = 77;
+
+    write_inode(&write_in);
+
+    in1 = iget(9);
+
+    CTEST_ASSERT(in1 != NULL);
+    CTEST_ASSERT(in1->inode_num == 9);
+    CTEST_ASSERT(in1->ref_count == 1);
+    CTEST_ASSERT(in1->size == 888);
+    CTEST_ASSERT(in1->block_ptr[0] == 77);
+
+    in2 = iget(9);
+
+    CTEST_ASSERT(in2 == in1);
+    CTEST_ASSERT(in1->ref_count == 2);
+
+    in1->size = 999;
+
+    iput(in1);
+    CTEST_ASSERT(in1->ref_count == 1);
+
+    iput(in1);
+    CTEST_ASSERT(in1->ref_count == 0);
+
+    read_inode(&read_in, 9);
+    CTEST_ASSERT(read_in.size == 999);
+
+    CTEST_ASSERT(image_close() == 0);
+
+    unlink(TEST_IMAGE);
+
+    return 0;
+}
+
 static int test_ialloc(void)
 {
     unsigned char block[BLOCK_SIZE];
@@ -175,6 +307,10 @@ int main(void)
     CTEST_RUN(test_block_write_read);
     CTEST_RUN(test_set_free);
     CTEST_RUN(test_find_free);
+    CTEST_RUN(test_incore_find_free);
+    CTEST_RUN(test_incore_find);
+    CTEST_RUN(test_read_write_inode);
+    CTEST_RUN(test_iget_iput);
     CTEST_RUN(test_ialloc);
     CTEST_RUN(test_alloc);
     CTEST_RUN(test_mkfs);
